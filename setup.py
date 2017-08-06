8fea348b5c3caec11dcd6504c53c023812c61f0a
Michael Liou <mliou112@yahoo.com>
2017-08-06 13:26:31 -0500

    dropped support for 2.6

--- setup.py
+++ setup.py
@@ -15,7 +15,6 @@ setup(
             'Development Status :: 4 - Beta',
             'Intended Audience :: Science/Research',
             'License :: OSI Approved :: MIT License',
-            'Programming Language :: Python :: 2.6',
             'Programming Language :: Python :: 2.7',
             'Programming Language :: Python :: 3.2',
             'Programming Language :: Python :: 3.3',
--- simulatesv/simulatesv.py
+++ simulatesv/simulatesv.py
@@ -8,7 +8,6 @@ Copyright © 2017 Michael Liou
 
 Permission is hereby granted, free of charge, to any person obtaining
 a copy of this software and associated documentation files (the "Software"),
-zo
 to deal in the Software without restriction, including without limitation
 the rights to use, copy, modify, merge, publish, distribute, sublicense,
 and/or sell copies of the Software, and to permit persons to whom the
